fd7b5c93bc68da1b13135683bc9665430b3ed327
= <=>
2019-06-21 19:33:42 +0530

    added loader and its functionality

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e) {
+  // hide results
+  document.getElementById('results').style.display = 'none';
+
+  // show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 1000);
+
+  e.preventDefault();
+});
 
 function calculateResults(e) {
-  e.preventDefault();
-  console.log('calculate');
-
   const amount = document.getElementById('amount'),
         interest = document.getElementById('interest'),
         years = document.getElementById('years'),
@@ -24,12 +31,25 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatePayments).toFixed(2);
     totalInterest.value = ((monthly * calculatePayments) - principal).toFixed(2);
+
+    // hide loader
+    document.getElementById('loading').style.display = 'none';
+    
+    // show results
+    document.getElementById('results').style.display = 'block';
   }else {
     showError('please check the numbers');
   }
 }
 
 function showError(err) {
+
+  // hide loader
+  document.getElementById('loading').style.display = 'none';
+  
+  // hide results
+  document.getElementById('results').style.display = 'none';
+
   // create a div
   const errorDiv = document.createElement('div');
 
